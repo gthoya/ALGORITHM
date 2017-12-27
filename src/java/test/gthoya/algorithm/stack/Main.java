afd624791ae9c1c5fb1565f9b28aed3833695a66
jacob <jacob.gt@kakaocorp.com>
2017-12-27 09:31:10 +0900

    백준 알고리즘 기본 세팅

---
+++ src/java/test/gthoya/algorithm/stack/Main.java
@@ -0,0 +1,10 @@
+package gthoya.algorithm.stack;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] ar) {
+        Scanner scanner = new Scanner(System.in);
+        int orderCount = scanner.nextInt();
+    }
+}
