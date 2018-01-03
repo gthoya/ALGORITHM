999c4d5c3204002313b9bf722df7bae8c0f08d7e
jacob <jacob.gt@kakaocorp.com>
2018-01-03 11:24:06 +0900

    백준 알고리즘 풀이용 클래스 초기화

--- src/java/test/gthoya/algorithm/Main.java
+++ src/java/test/gthoya/algorithm/Main.java
@@ -5,35 +5,5 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] ar) {
         Scanner scanner = new Scanner(System.in);
-        int minNumber = scanner.nextInt();
-        int maxNumber = scanner.nextInt();
-        int maxPrimeNumber = 2;
-
-        boolean[] checkNotPrimeNumber = new boolean[maxNumber + 1];
-        checkNotPrimeNumber[0] = true;
-        checkNotPrimeNumber[1] = true;
-
-        for (int i = 2; i <= maxNumber; i++) {
-            if (checkNotPrimeNumber[i] == true) {
-                continue;
-            }
-
-            for (int j = maxPrimeNumber; j <= i; j++) {
-                if (i % j == 0) {
-                    maxPrimeNumber = j;
-                    for (int k = j * 2; k <= maxNumber; k += j) {
-                        checkNotPrimeNumber[k] = true;
-                    }
-                }
-            }
-        }
-
-        for (int i = minNumber; i <= maxNumber; i++) {
-            if (checkNotPrimeNumber[i] == true) {
-                continue;
-            }
-
-            System.out.println(i);
-        }
     }
 }
