8cfc847c7671680d18ee1d2bf238c998080e601a
jacob <jacob.gt@kakaocorp.com>
2018-05-08 17:26:07 +0900

    software expert academy level3 - day1 : 1206

--- src/java/test/gthoya/algorithm/Main.java
+++
@@ -1,9 +0,0 @@
-package gthoya.algorithm;
-
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] ar) {
-        Scanner scanner = new Scanner(System.in);
-    }
-}
