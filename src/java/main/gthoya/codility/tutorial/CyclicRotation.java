6049fa0e1dc1faaea0e56c648ca5ebd7c7977ed5
gthoya <gthoya85@hanmail.net>
2019-09-03 22:31:46 +0900

    codility - tutorial - CyclicRotation

---
+++ src/java/main/gthoya/codility/tutorial/CyclicRotation.java
@@ -0,0 +1,25 @@
+package gthoya.codility.tutorial;
+
+public class CyclicRotation {
+    public int[] solution(int[] A, int K) {
+        if (A == null || A.length == 0) {
+            return A;
+        }
+
+        int arrSize =  A.length;
+        int move = K % arrSize;
+        int[] result = new int[arrSize];
+
+        for (int i = 0; i < arrSize; i++) {
+            result[i] = A[(i + arrSize - move) % arrSize];
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        CyclicRotation cyclicRotation = new CyclicRotation();
+
+        System.out.println(cyclicRotation.solution(new int[]{3, 8, 9, 7, 6}, 3));
+    }
+}
